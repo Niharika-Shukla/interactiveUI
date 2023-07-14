ddc668a19adbe0994fa503651754aad1ada56cf8
Niharika <innovation.niharika@gmail.com>
2023-07-14 10:16:41 -0400

    Initial Commit

---
+++ interactiveUI/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  interactiveUI
+//
+//  Created by Niharika Shukla on 7/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
+    
+    var body: some View {
+        VStack {
+            Text(textTitle)
+                .font(.title)
+            TextField("Type name here...", text: $name)
+                .multilineTextAlignment(.center)
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .border(/*@START_MENU_TOKEN@*/Color.gray/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                .cornerRadius(5.0)
+            Button("Submit name") {
+                textTitle = "Welcome, \(name)!"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+        }.padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ interactiveUI/interactiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactiveUIApp.swift
+//  interactiveUI
+//
+//  Created by Niharika Shukla on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct interactiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
